409a43a246b53be6df6ef0ecb0f4305d230c5e2c
koenvinken <koen.vinken@wijs.be>
2014-09-14 15:27:21 +0200

    add caption to datagrid, remove title

--- src/Backend/Modules/Slideshow/Actions/Edit.php
+++ src/Backend/Modules/Slideshow/Actions/Edit.php
@@ -237,7 +237,7 @@ class Edit extends BackendBaseActionEdit
             'checkbox',
             'preview',
             'hidden',
-            'title',
+            'caption',
             'edit'
         );
 
