6a131a2130bf0840e070101893da10e98388b1a4
koenvinken <koen.vinken@wijs.be>
2014-09-27 11:10:49 +0200

    extra_id allow null

--- src/Backend/Modules/Slideshow/Installer/Data/install.sql
+++ src/Backend/Modules/Slideshow/Installer/Data/install.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `slideshow_galleries` (
  `user_id` int(11) NOT NULL,
  `meta_id` int(11) NOT NULL,
  `category_id` int(11) NOT NULL,
- `extra_id` int(4) NOT NULL,
+ `extra_id` int(4) NULL,
  `language` varchar(5) NOT NULL,
  `title` varchar(255) NOT NULL,
  `description` text,
